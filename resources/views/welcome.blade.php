fc2abb5183d63ef0a8b9d8c9992d2a1f46b6bc32
Ayrat <ayrat.2013@ya.ru>
2020-05-14 22:46:20 +0300

    view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2020
+                    Laravel 202
                 </div>
 
                 <div class="links">
