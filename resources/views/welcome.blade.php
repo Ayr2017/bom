844b1c3b8461411452c647089f8f85dbcd91dd85
Ayrat <ayrat.2013@ya.ru>
2020-05-14 22:51:53 +0300

    als

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2020
+                    Laravel 2020 Alsu
                 </div>
 
                 <div class="links">
